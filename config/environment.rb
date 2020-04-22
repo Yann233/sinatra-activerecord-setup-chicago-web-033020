3e52a983593981bc298bd251a0f05935428cee24
Yann Xiaoyan Yang <yannxyang233@gmail.com>
2020-04-22 19:59:16 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,16 @@
+# We now have access to all of the gems that we need, but we still need to set up a connection to our database.
+# Add the following block of code to your environment.rb file
+# (underneath Bundler.require(:default, ENV['SINATRA_ENV'])).
+
+
+
 ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-
+##########
+configure :development do
+  set :database, 'sqlite3:db/database.db'
+end
+############
 require './app'
